86ab46551a933200a46f5ee7fed15e324a99689c
iamyujinwon <iamyujinwon@gmail.com>
2021-11-05 17:46:07 -0400

    add changes

---
+++ src/app/album/album.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import albumData from '../data/SearchResultsAlbum.json';
+
+@Component({
+  selector: 'app-album',
+  templateUrl: './album.component.html',
+  styleUrls: ['./album.component.css']
+})
+export class AlbumComponent implements OnInit {
+
+  album:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.album = albumData
+  }
+
+}
