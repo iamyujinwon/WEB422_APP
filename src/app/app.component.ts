0e4ce007718cdff88eb3d9b8ede2b139237e9110
iamyujinwon <iamyujinwon@gmail.com>
2021-11-05 17:53:41 -0400

    add declaration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 04
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Yujin Won  Student ID: 153565197  Date: 2021-11-05
+*
+********************************************************************************/
+
 import { Component } from '@angular/core';
 
 @Component({
